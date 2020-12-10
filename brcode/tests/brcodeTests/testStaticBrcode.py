9ab6d15f1241f0cc4d888e6c9afb8c88bcf46adb
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-12-10 11:50:47 -0300

    Update tests

--- brcode/tests/brcodeTests/testStaticBrcode.py
+++ brcode/tests/brcodeTests/testStaticBrcode.py
@@ -3,16 +3,16 @@ from unittest import TestCase
 from brcode.static import fromJson
 
 
-class TestDynamicfromJson(TestCase):
+class TestStaticfromJson(TestCase):
 
     def test_success(self):
         json = {
             "key": "arya@stark.com",
             "amount": 12500,
             "name": "Arya Stark with a very long name",
-            "city": "São Paulo",
+            "city": "Pindamonhangaba do Norte do Sul de Winterfell",
             "txid": "verylongreconciliationidtotestfieldlength"
         }
         actualBrcode = fromJson(json)
-        expectedBrcode = "00020126360014br.gov.bcb.pix0114arya@stark.com5204000053039865406125.005802BR5925Arya Stark with a very lo6009Sao Paulo62290525verylongreconciliationidt630466F4"
-        self.assertEqual(actualBrcode, expectedBrcode)
+        expectedBrcode = "00020126360014br.gov.bcb.pix0114arya@stark.com5204000053039865406125.005802BR5925Arya Stark with a very lo6015Pindamonhangaba62290525verylongreconciliationidt630405B7"
+        self.assertEqual(expectedBrcode, actualBrcode)
